bd6041268e6117236a7c7a637f2d49c932466a0a
sofi-gonzalez <sofi.agz85@gmail.com>
2021-09-20 20:38:50 -0300

    Actualizacion de Entrega

--- src/main/java/com/example/demo/model/Paciente.java
+++ src/main/java/com/example/demo/model/Paciente.java
@@ -2,15 +2,15 @@ package com.example.demo.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Set;
 
 @Entity
-@Getter @Setter
+@Table(name = "Pacientes")
+@Getter
+@Setter
 public class Paciente {
 
     @Id
